00024f69db2439a6fd34b8403d6eea5e01b7e83f
Oussama <Hassouneoussama8@gmail.com>
2025-11-11 12:44:42 +1100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,62 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <div className="bg-background h-screen">
+      <section className="relative hidden md:block">
+        <img src="/Hero10.png" alt="Hero" className="m-auto mt-40 w-full" />
+        <div className="absolute -bottom-14 left-14 z-0 mt-8 max-w-[40%] bg-white p-8 xl:p-12 2xl:p-20">
+          <h1 className="text-foreground mb-4 text-[2.5vw] leading-tight font-bold xl:mb-6 xl:text-[2.2vw] 2xl:mb-8 2xl:text-[3vw]">
+            Global Freight & Logistics Solutions
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+          <h2 className="text-foreground text-sm leading-relaxed xl:text-base 2xl:text-lg">
+            Connecting Australia to the world through reliable sea and air
+            freight, customs clearance, and specialised logistics services with
+            Orrsman Group
+          </h2>
+          <div className="mt-6 flex gap-3 xl:mt-8 2xl:mt-10">
+            <Button className="p-4 text-base font-semibold xl:p-5 xl:text-lg">
+              Our services
+            </Button>
+            <Button
+              variant="outline"
+              className="p-4 text-base font-semibold xl:p-5 xl:text-lg"
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+              Get Quote
+            </Button>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      </section>
+      <section className="relative md:hidden">
+        <img
+          src="/MobileHero2.png"
+          alt="MobileHero.png"
+          className="mt-40 w-full"
+        />
+        <div className="@container">
+          <div className="absolute -bottom-75 left-1/2 z-0 mt-8 min-w-[80%] -translate-x-1/2 bg-white p-6 xl:p-12 2xl:p-20 @[400]:-bottom-55">
+            <h1 className="text-foreground mb-4 text-3xl leading-tight font-bold @[600]:text-4xl">
+              Global Freight &
+              <br />
+              Logistics Solutions
+            </h1>
+            <h2 className="text-foreground text-sm leading-relaxed @[600]:text-[18px]">
+              Connecting Australia to the world through reliable sea and air
+              freight, customs clearance, and specialised logistics services
+              with Orrsman Group
+            </h2>
+            <div className="mt-6 flex gap-3 xl:mt-8 2xl:mt-10">
+              <Button className="p-4 text-base font-semibold">
+                Our services
+              </Button>
+              <Button variant="outline" className="p-4 text-base font-semibold">
+                Get Quote
+              </Button>
+            </div>
+          </div>
         </div>
-      </main>
+      </section>
     </div>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { Menu } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+export default function Header() {
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <nav
+      className={`fixed top-0 left-1/2 z-50 mt-7 flex w-11/12 max-w-7xl -translate-x-1/2 flex-col justify-center bg-white/40 py-5 backdrop-blur-lg md:rounded-full ${isOpen ? "rounded-sm" : "rounded-full"}`}
+    >
+      <div className="flex items-center justify-between">
+        <div className="mx-10 flex w-full items-center justify-between">
+          <Image src="/LogoOG2.png" alt="logo" width={60} height={60} />
+          <div className="mr-20 hidden space-x-5 font-semibold text-black md:block">
+            <Link href="/">Home</Link>
+            <Link href="/">Services</Link>
+            <Link href="/">About us</Link>
+            <Link href="/">Contact us</Link>
+          </div>
+        </div>
+        <div className="pr-8 md:hidden">
+          <Button onClick={() => setIsOpen(!isOpen)}>
+            <Menu className="size-5" />
+          </Button>
+        </div>
+      </div>
+      {isOpen && (
+        <div className="flex flex-col items-center justify-center gap-3 px-5 py-3 md:hidden">
+          <Link href="/">Home</Link>
+          <Link href="/">Services</Link>
+          <Link href="/">About us</Link>
+          <Link href="/">Contact us</Link>
+        </div>
+      )}
+    </nav>
+  );
+}
